d3c1cc40399823f824b9e7531785ad5fe038d888
easy909 <1730127479@qq.com>
2020-08-12 15:39:52 +0800

    侧边栏路由点击收起

--- src/views/Index/components/header.js
+++ src/views/Index/components/header.js
@@ -1,17 +1,37 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { UnorderedListOutlined } from "@ant-design/icons";
 import "./asice.scss";
 class AsideHeader extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            collapsed: props.collapsed
+        };
     }
-
+    toggleMenu = () => {
+        this.props.toggle();
+    }
     render() {
+        const { collapsed } = this.state;
         return (
-            <Fragment>
-                  <h1 className="logo"><span>Logo</span></h1>  
-            </Fragment>
+            <div className={collapsed ? "collapsed-close" : ""}>
+                <h1 className="logo"><span>Logo</span></h1>
+                <div className="header-wrap">
+                    <span className="collapsed-icon" onClick={this.toggleMenu}><UnorderedListOutlined /></span>
+                </div>
+            </div>
         );
+    }
+    static getDerivedStateFromProps(nextProps, prevState) {
+        const { collapsed } = nextProps;
+        // 当传入的type发生变化的时候，更新state
+        if (collapsed !== prevState.collapsed) {
+            return {
+                collapsed
+            }
+        }
+        // 否则，对于state不进行任何操作
+        return null;
     }
 }
 
--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -8,33 +8,42 @@ const { Header, Sider, Content } = Layout;
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            collapsed: false
+        };
     }
-    state = {
-        collapsed: false,
-    };
+
     toggle = () => {
+        const collapsed = !this.state.collapsed;
         this.setState({
-            collapsed: !this.state.collapsed,
+            collapsed
         });
+        sessionStorage.setItem("collapsed",collapsed);
     };
+
     render() {
         return (
             <Layout className="layout-wrap">
                 <Header className="layout-header">
-                    <AsideHeader/>
+                    <AsideHeader toggle={this.toggle} collapsed={this.state.collapsed} />
                 </Header>
 
                 <Layout>
-                    <Sider width="250px">
+                    <Sider width="250px" collapsed={this.state.collapsed}>
                         <Aside />
                     </Sider>
                     <Content className="layout-main">
-                        <Container/>
+                        <Container />
                     </Content>
                 </Layout>
             </Layout>
         );
+    }
+    componentDidMount() {
+        const collapsed = JSON.parse(sessionStorage.getItem("collapsed"));
+        this.setState({
+            collapsed
+        });
     }
 }
 
